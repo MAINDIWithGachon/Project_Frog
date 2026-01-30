888659f70ba8235e630e82a184fb118c87ec6497
sehoon <aabb0125@naver.com>
2026-01-30 15:36:21 +0900

    C# 테스트 코드 작성 완료

--- Assets/Scripts/test_123.cs
+++ Assets/Scripts/test_123.cs
@@ -5,7 +5,7 @@ public class test_123 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Console.Write("Test");
     }
 
     // Update is called once per frame
