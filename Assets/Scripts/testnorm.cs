ab6e8cf5bba42ff1fd42fc6b78e5af8f079af36c
normer22 <nadasky33@gmail.com>
2026-02-05 13:56:38 +0900

    testnorm

---
+++ Assets/Scripts/testnorm.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class testnorm : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // aaaaaaaaaaaaaaaaaaaaaaaaaaaaa test
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
