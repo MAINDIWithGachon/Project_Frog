2624da68aa38c335ab376c5c2d00448fdb4ac71c
seungki <rlatmdrl4512@daum.net>
2026-04-08 20:32:57 +0900

    <feat> 재화량 UI 표시
    <feat> 재화량 UI 표시

---
+++ Assets/Scripts/UI/MainCurrencyUI.cs
@@ -0,0 +1,51 @@
+using TMPro;
+using UnityEngine;
+
+public class MainCurrencyUI : MonoBehaviour
+{
+    [Header("# Reference")]
+    [SerializeField] private RuntimeData runtimeData;
+
+    [Header("# UI")]
+    [SerializeField] private TMP_Text goldText;
+    [SerializeField] private TMP_Text gemText;
+
+    private void Awake()
+    {
+        ResolveReferences();
+    }
+
+    private void OnEnable()
+    {
+        ResolveReferences();
+
+        if (runtimeData != null)
+            runtimeData.OnDataChanged += RefreshCurrencyUI;
+
+        RefreshCurrencyUI();
+    }
+
+    private void OnDisable()
+    {
+        if (runtimeData != null)
+            runtimeData.OnDataChanged -= RefreshCurrencyUI;
+    }
+
+    public void RefreshCurrencyUI()
+    {
+        if (runtimeData == null)
+            return;
+
+        if (goldText != null)
+            goldText.text = runtimeData.GetGold().ToString();
+
+        if (gemText != null)
+            gemText.text = runtimeData.GetUpgradeStone().ToString();
+    }
+
+    private void ResolveReferences()
+    {
+        if (runtimeData == null)
+            runtimeData = FindAnyObjectByType<RuntimeData>();
+    }
+}
