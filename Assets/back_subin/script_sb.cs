a113d6bfc095a3e8b2bebd4a6a8d7df4b59a1f08
sim68652 <sim68652@gmail.com>
2026-05-01 17:23:56 +0900

    void
    void

--- Assets/back_subin/script_sb.cs
+++ Assets/back_subin/script_sb.cs
@@ -5,11 +5,16 @@ public class script_sb : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("test 1234");
+        test();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    void test()
     {
         
     }
